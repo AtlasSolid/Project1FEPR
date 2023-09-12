a3668eb96cc34aef1f7a82b9b3c48914df8bcc9e
Noah Wilson <nwilson0603@gmail.com>
2023-09-12 16:27:26 -0500

    fixed error message bug, reworking fork/exec

--- main.cpp
+++ main.cpp
@@ -18,10 +18,35 @@ int main(int argc, char * argv[]) {
     char * fileName = nullptr;
 
     //if no arguments provided, print error message, do not fork, do not pipe, do not exec, end process here
-    if (!HandleOptions(argc, argv, &pathName, &fileName)) {
+    if (HandleOptions(argc, argv, &pathName, &fileName) == false) {
 		printError(); //this keeps executing even when the right arguments are given and i'm not sure why
 		return 1;
 	}
+
+    //int p[2];
+    //for implementing pipes
+
+    char *myargv[2]; //array of char pointers
+
+    int rc = fork(); //initiate fork
+
+        if (rc < 0) // if the fork failed, print error and exit
+        { 
+            fprintf(stderr, "fork failed\n");
+
+            exit(1); 
+        }
+        else if (rc == 0) //if fork was successful
+        {
+            printf("Child 1: pid:%d\n", (int) getpid()); 
+
+            myargv[0] = pathName; //program to execute, either ls or wc depending on cmd line args
+            myargv[1] = 0; //file that ls or wc would be reading from, main.cpp is placeholder. -o and -a would replace this value with whatever file we want to read
+
+            execvp(pathName, myargv); //first draft of exec call, also checks if the path leads to an executable file
+
+            cout << "Failed exec call" << endl; //this line shouldn't execute unless execvp() screwed up
+        }
     
 
     return 0;
@@ -38,37 +63,16 @@ void printError()
 bool HandleOptions(int argc, char ** argv, char** pName, char** fName) {
     int c;
 
-    //int p[2];
-    //for implementing pipes
-
-    char *myargv[2]; //array of char pointers
+    bool argTrue = false;
 
     //checks args to see if they mach any valid command line options, execute switch statements if this is the case
     while ((c = getopt(argc, argv, "1:dt:vi:o:a:2:")) != -1) {
+        argTrue = true;
+
 		switch (c) {
             {
                 case '1': //if case '-1' is present
                 *pName = optarg;
-
-                int rc = fork(); //initiate fork
-
-                if (rc < 0) // if the fork failed, print error and exit
-                { 
-                    fprintf(stderr, "fork failed\n");
-
-                    exit(1); 
-                }
-                else if (rc == 0) //if fork was successful
-                {
-                    printf("Child 1: pid:%d\n", (int) getpid()); 
-
-                    myargv[0] = *pName; //program to execute, either ls or wc depending on cmd line args
-                    myargv[1] = *fName; //file that ls or wc would be reading from, main.cpp is placeholder. -o and -a would replace this value with whatever file we want to read
-
-                    execvp(*pName, fName); //first draft of exec call, also checks if the path leads to an executable file
-
-                    cout << "Failed exec call" << endl; //this line shouldn't execute unless execvp() screwed up
-                }
 
             }
 
@@ -86,6 +90,6 @@ bool HandleOptions(int argc, char ** argv, char** pName, char** fName) {
 
     }
 
-    return 0;
+    return argTrue;
 
 }
