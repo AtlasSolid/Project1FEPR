cdc2fb1661ad74ee381a5d588b67329424e0bd88
Noah Wilson <nwilson0603@gmail.com>
2023-09-14 21:27:58 -0500

    WEXITSTATUS implemented, open() checks implemented

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <filesystem>
@@ -42,7 +42,6 @@ int main(int argc, char * argv[]) {
     if (secondPathName == 0) //if '-2' is not in the arguments
     {
         pid_t rc = fork(); //parent forks child 1
-        int cVal = 0;
 
         if (rc < 0) // if the fork failed, print error and exit
         { 
@@ -52,17 +51,24 @@ int main(int argc, char * argv[]) {
         }
         else if (rc == 0) //if fork was successful
         {
-            cVal = rc;
 
             myargv[0] = strdup(pathName); //program to execute, either ls or wc depending on cmd line args
             myargv[1] = 0; //file that ls or wc would be reading from
 
-            if (fileName != 0)
+            if (fileName != 0) //if a file name was passed
             {
-                int fd = open(fileName, S_IRUSR);
-                close(0);
-                dup(fd);
-                close(fd);
+                int fd = open(fileName, S_IRUSR); //wiring -i's file path to the standard input
+                if (fd > 0) //if open() was successful
+                {
+                    close(STDIN_FILENO);
+                    dup(fd);
+                    close(fd);
+                }
+                else
+                {
+                    fprintf(stderr, "Invalid file\n");
+                    exit(1);
+                }
             }
             
             execvp(myargv[0], myargv); //executes ls or wc, also checks if the path leads to an executable file
@@ -71,8 +77,10 @@ int main(int argc, char * argv[]) {
         }
         else //parent goes down this path
         {
-            wait(NULL); //parent waits for child process to finish, i.e. waits for execvp to properly output to the console
-            printf("Child 1: %d returns: %d\n", rc, cVal);
+            int wstatus;
+
+            waitpid(rc, &wstatus, 0); //wait until c1 is complete
+            printf("Child 1: %d returns: %d\n", rc, WEXITSTATUS(wstatus));
         }
 
     }
@@ -83,9 +91,6 @@ int main(int argc, char * argv[]) {
 
         int rc = 0;
         int rc2 = 0;
-
-        int cVal1 = 0;
-        int cVal2 = 0;
 
         int pFail = pipe(p);
 
@@ -107,18 +112,13 @@ int main(int argc, char * argv[]) {
             }
             else if (rc == 0)
             {
-                cVal1 = rc;
 
                 close(p[0]); //not reading from the pipe, so close read side
-                close(1); //close STDOUT to make it available
+                close(STDOUT_FILENO); //close STDOUT to make it available
 
                 dup(p[1]); //dup write side of the pipe
 
                 close(p[1]); //close original write side of pipe, connection still in File Descriptor 1
-
-                //printf("Child 1: pid:%d\n", (int) getpid()); 
-                
-                //cout << "Child 1: " << getpid() << endl;
 
                 myargv[0] = strdup(pathName);
                 myargv[1] = 0;
@@ -126,9 +126,17 @@ int main(int argc, char * argv[]) {
                 if (fileName != 0) //if -i case present, wire -i file into standard input, first in the sequence
                 {
                     int fd = open(fileName, O_SYNC, S_IRUSR); //read mode
-                    close(0);
-                    dup(fd);
-                    close(fd);
+                    if (fd > 0)
+                    {
+                        close(STDIN_FILENO);
+                        dup(fd);
+                        close(fd);
+                    }
+                    else
+                    {
+                        fprintf(stderr, "Invalid file\n");
+                        exit(1);
+                    }
                 }
                 execvp(myargv[0], myargv);
 
@@ -150,16 +158,13 @@ int main(int argc, char * argv[]) {
             }
             else if (rc2 == 0)
             {
-                cVal2 = rc2;
 
                 close(p[1]); //close write side of the pipe
-                close(0); //close STDIN to make it available
+                close(STDIN_FILENO); //close STDIN to make it available
 
                 dup(p[0]); //dup read side of the pipe
 
                 close(p[0]); //close original read side of pipe, connection still in File Descriptor 0
-
-                //cout << "Child 2: " << getpid() << endl; 
 
                 myargv[0] = strdup(secondPathName);
                 myargv[1] = 0;
@@ -167,33 +172,51 @@ int main(int argc, char * argv[]) {
                 if (oFileName != 0) //if -o case present, wire -o file into standard output, last in the sequence
                 {
                     int fd2 = open(oFileName, O_SYNC, S_IRWXU); //overwrite mode
-                    close(1);
-                    dup(fd2);
-                    close(fd2);
+                    if (fd2 > 0)
+                    {
+                        close(STDOUT_FILENO);
+                        dup(fd2);
+                        close(fd2);
+                    }
+                    else
+                    {
+                        fprintf(stderr, "Invalid file\n");
+                        exit(1);
+                    }
                 }
                 else if (aFileName != 0) //if -a case present, wire -a file into standard output, last in the sequence
                 {
                     int fd2 = open(aFileName, O_APPEND, S_IRWXU); //append mode
-                    close(1);
-                    dup(fd2);
-                    close(fd2);   
+                    if (fd2 > 0)
+                    {
+                        close(STDOUT_FILENO);
+                        dup(fd2);
+                        close(fd2);
+                    }
+                    else
+                    {
+                        fprintf(stderr, "Invalid file\n");
+                        exit(1);
+                    } 
                 }
 
                 execvp(myargv[0], myargv);
 
-                cout << "Failed exec call" << endl; //this line shouldn't execute unless execvp() screwed up
+                cout << "Failed exec call" << endl;
             }
             else
             {
 
             }
 
+            int wstatus;
+
             close(p[0]);
             close(p[1]); //closing read and write sides of pipe
-            waitpid(rc, NULL, 0); //wait until c1 is complete
-            waitpid(rc2, NULL, 0); //wait until c2 is complete
-            printf("Child 2: %d returns: %d\n", rc2, cVal2);
-            printf("Child 1: %d returns: %d\n", rc, cVal1);
+            waitpid(rc, &wstatus, 0); //wait until c1 is complete
+            printf("Child 1: %d returns: %d\n", rc, WEXITSTATUS(wstatus));
+            waitpid(rc2, &wstatus, 0); //wait until c2 is complete
+            printf("Child 2: %d returns: %d\n", rc2, WEXITSTATUS(wstatus));
 
         }
     
